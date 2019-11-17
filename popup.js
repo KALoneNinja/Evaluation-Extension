c1b3cf5b047bf60adf041c3c0d17e1e64cdc6b83
KALoneNinja <53975109+KALoneNinja@users.noreply.github.com>
2019-11-16 18:27:16 -0600

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-$("#next-eval").on("click", function(){
-    
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "popup_message_attempt"});
-      });
-    
-});
